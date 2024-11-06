3acbd2c435c88e01490eff8aaffdb61fc7469d02
aldinalj <aldinalj@gmail.com>
2024-11-06 19:31:48 +0100

    initial commit

---
+++ src/main/java/com/aldinalj/enterprise_project/EnterpriseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aldinalj.enterprise_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aldinalj/enterprise_project/EnterpriseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldinalj.enterprise_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
