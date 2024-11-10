9c4d45ac742fd8c303329bcc01e42b90e4c41987
aldinalj <aldinalj@gmail.com>
2024-11-10 15:59:37 +0100

    Add UserRepository

---
+++ src/main/java/com/aldinalj/enterprise_project/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aldinalj.enterprise_project.user.repository;
+
+import com.aldinalj.enterprise_project.user.model.CustomUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<CustomUser, Long> {
+}
