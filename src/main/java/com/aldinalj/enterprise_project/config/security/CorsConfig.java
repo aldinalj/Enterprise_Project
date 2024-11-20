23c339c0a3cd87dfa8aeaa1e24d43552c48d4283
aldinalj <aldinalj@gmail.com>
2024-11-20 19:08:26 +0100

    Add an endpoint for testing

--- src/main/java/com/aldinalj/enterprise_project/config/security/CorsConfig.java
+++ src/main/java/com/aldinalj/enterprise_project/config/security/CorsConfig.java
@@ -31,5 +31,6 @@ public class CorsConfig {
 
         return source;
 
+
     }
 }
--- src/main/java/com/aldinalj/enterprise_project/user/controller/UserController.java
+++ src/main/java/com/aldinalj/enterprise_project/user/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.aldinalj.enterprise_project.user.controller;
 
+import com.aldinalj.enterprise_project.config.security.CustomUserDetails;
+import com.aldinalj.enterprise_project.user.model.CustomUser;
 import com.aldinalj.enterprise_project.user.model.dto.CustomUserDTO;
 import com.aldinalj.enterprise_project.user.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import static com.aldinalj.enterprise_project.user.authorities.UserRole.USER;
 
 @RestController
 @RequestMapping("/user")
@@ -31,5 +36,31 @@ public class UserController {
     public ResponseEntity<CustomUserDTO> deleteAccount(@AuthenticationPrincipal UserDetails userDetails) {
 
         return userService.deleteUser(userDetails);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<CustomUserDTO> testFetchUser(@AuthenticationPrincipal UserDetails userDetails) {
+
+        /*System.out.println(userDetails.getUsername() + "/n"
+                + userDetails.getPassword() + "/n"
+                + userDetails.getAuthorities());
+        ResponseEntity<UserDetails> response = ResponseEntity.ok(userDetails);
+        System.out.println(response);*/
+
+        if (userDetails != null) {
+            CustomUserDTO customUerDTO = new CustomUserDTO(
+                    userDetails.getUsername(),
+                    userDetails.getPassword()
+            );
+
+            System.out.println("User " + customUerDTO.username() + " is authenticated");
+
+            return ResponseEntity.ok(customUerDTO);
+        } else {
+
+            return ResponseEntity.ok().body(new CustomUserDTO("clarkkent", "superman"));
+
+        }
+
     }
 }
